0fc3bd166adf1d31ecae0d7f33c752cd0f5d9e54
Yannic-reust <62382435+Yannic-reust@users.noreply.github.com>
2025-05-15 09:27:28 +0200

    feat: add login & register page mock

--- apps/dashboard-frontend/src/App.jsx
+++ apps/dashboard-frontend/src/App.jsx
@@ -2,11 +2,15 @@
 import { Routes, Route } from "react-router-dom";
 import Products from "./pages/products";
 import Projects from "./pages/projects";
+import Register from "./pages/register";
+import SignIn from "./pages/signIn";
 
 const App = () => {
   return (
     <>
       <Routes>
+        <Route path="/login" element={<SignIn />} />
+        <Route path="/register" element={<Register />} />
         <Route path="/projects" element={<Projects />} />
         <Route path="/products" element={<Products />} />
       </Routes>
