31dac4d09e9229314342bac9b94704c9113bf2a5
Yannic-reust <62382435+Yannic-reust@users.noreply.github.com>
2025-05-31 14:31:45 +0200

    feat: improve feedback modal on dashboard

--- apps/dashboard-frontend/src/components/detailsProject/feedbacksOverview/feedbackDetailsModal/feedbackDetailsModal.jsx
+++ apps/dashboard-frontend/src/components/detailsProject/feedbacksOverview/feedbackDetailsModal/feedbackDetailsModal.jsx
@@ -81,16 +81,24 @@ export default function feedbackDetailsModal({
     <Dialog open={open} onClose={close} className="relative z-10">
       <DialogBackdrop
         transition
-        className="fixed inset-0 bg-gray-500/75 transition-opacity data-closed:opacity-0 data-enter:duration-300 data-enter:ease-out data-leave:duration-200 data-leave:ease-in"
+        className="fixed inset-0 bg-black/10 transition-opacity data-closed:opacity-0 data-enter:duration-300 data-enter:ease-out data-leave:duration-200 data-leave:ease-in"
       />
 
       <div className="fixed inset-0 z-10 w-screen overflow-y-auto">
         <div className="flex min-h-full items-end justify-center p-4 text-center sm:items-center sm:p-0">
           <DialogPanel
             transition
-            className="relative min-w-xl min-h-96 transform overflow-hidden rounded-lg bg-white text-black text-left shadow-xl transition-all data-closed:translate-y-4 data-closed:opacity-0 data-enter:duration-300 data-enter:ease-out data-leave:duration-200 data-leave:ease-in sm:my-8 sm:w-full sm:max-w-sm sm:p-6 data-closed:sm:translate-y-0 data-closed:sm:scale-95"
+            className="relative min-w-3xl min-h-96 transform overflow-hidden rounded-lg bg-white text-black text-left shadow-xl transition-all data-closed:translate-y-4 data-closed:opacity-0 data-enter:duration-300 data-enter:ease-out data-leave:duration-200 data-leave:ease-in sm:my-8 sm:w-full sm:max-w-sm sm:p-6 data-closed:sm:translate-y-0 data-closed:sm:scale-95"
           >
             <div className="flex items-start justify-between">
+              <div className="absolute top-2 right-2">
+                <img
+                  src={crossIcon}
+                  alt="Close Icon"
+                  className="w-6 h-6 cursor-pointer"
+                  onClick={() => close()}
+                />
+              </div>
               <div className="">
                 <DialogTitle as="h3" className=" font-semibold  text-h-xs">
                   {feedback.title}
@@ -110,12 +118,14 @@ export default function feedbackDetailsModal({
                   )}
                 </div>
               </div>
-              <FeedbackStatusDropdown
-                status={feedback.status}
-                setStatus={(newStatus) =>
-                  handleChangeStatus(newStatus, feedback.id)
-                }
-              />
+              <div className="mt-8">
+                <FeedbackStatusDropdown
+                  status={feedback.status}
+                  setStatus={(newStatus) =>
+                    handleChangeStatus(newStatus, feedback.id)
+                  }
+                />
+              </div>
             </div>
 
             <div className="flex justify-between items-end mt-4">
@@ -199,7 +209,7 @@ export default function feedbackDetailsModal({
                 </div>
               </form>
             )}
-            <div className="mt-6 max-h-108 overflow-y-scroll">
+            <div className="mt-6 max-h-72 overflow-y-scroll">
               <p>Comments</p>
               {feedback.comments.length === 0 && (
                 <p className="text-p-sm">No comments yet.</p>
--- apps/overlay-frontend/src/components/extendedOverlayModal.jsx
+++ apps/overlay-frontend/src/components/extendedOverlayModal.jsx
@@ -7,6 +7,7 @@ import {
   DialogPanel,
   DialogTitle,
 } from "@headlessui/react";
+import crossIcon from "../assets/icons/cross_icon.svg";
 import axios from "axios";
 
 export default function extendedOverlayModal({ open, close, htmlElement }) {
@@ -72,8 +73,16 @@ export default function extendedOverlayModal({ open, close, htmlElement }) {
         <div className="flex min-h-full items-end justify-center p-4 text-center sm:items-center sm:p-0 ">
           <DialogPanel
             transition
-            className="relative transform overflow-hidden min-w-lg rounded-lg bg-white px-4 pt-5 pb-4 text-left shadow-xl transition-all data-closed:translate-y-4 data-closed:opacity-0 data-enter:duration-300 data-enter:ease-out data-leave:duration-200 data-leave:ease-in sm:my-8 sm:w-full sm:max-w-sm sm:p-6 data-closed:sm:translate-y-0 data-closed:sm:scale-95"
+            className="relative transform overflow-hidden min-w-lg rounded-lg bg-white p-10 text-left shadow-xl transition-all data-closed:translate-y-4 data-closed:opacity-0 data-enter:duration-300 data-enter:ease-out data-leave:duration-200 data-leave:ease-in  sm:w-full sm:max-w-sm data-closed:sm:translate-y-0 data-closed:sm:scale-95"
           >
+            <div className="absolute top-2 right-2">
+              <img
+                src={crossIcon}
+                alt="Close Icon"
+                className="w-6 h-6 cursor-pointer"
+                onClick={() => close()}
+              />
+            </div>
             <form onSubmit={handleSubmit}>
               <div className="space-y-4">
                 <input
